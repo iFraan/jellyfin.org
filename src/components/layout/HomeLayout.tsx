10a63b94ed912879c2764136f4dbff80080fae71
Francisco Zorat <franciscozorat@gmail.com>
2024-06-04 10:29:44 -0300

    replace in index

--- src/components/layout/HomeLayout.tsx
+++ src/components/layout/HomeLayout.tsx
@@ -3,14 +3,16 @@ import styles from './HomeLayout.module.scss';
 import Navbar from './Navbar';
 
 type Props = {
-    children: ReactNode | ReactNode[] | undefined;
+    children?: ReactNode | ReactNode[];
 }
 
 export default function HomeLayout({ children }: Props) {
     return (
         <div className={styles['main-wrap']}>
             <Navbar />
-            {children}
+            <main>
+                {children}
+            </main>
             {/* Footer */}
         </div>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@ import Logo from '@theme/Logo';
 import styles from './index.module.scss';
 import LogoWhiteNoText from '../../static/images/logo-white-notext.svg';
 import ArrowDownIcon from '../../static/images/icons/arrow-down.svg';
+import HomeLayout from '../components/layout/HomeLayout';
 
 const links = [
   { href: '/blog', name: 'Blog' },
@@ -26,88 +27,35 @@ export default function Home() {
       title={`The Free Software Media System`}
       description='The volunteer-built media solution that puts you in control of your media. Stream to any device from your own server, with no strings attached.'
     >
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-          paddingTop: '0.5em',
-          paddingBottom: '0.5em',
-          backgroundColor: '#000012'
-        }}
-      >
-        <div>
-          <div style={{ width: '7em' }}>
-            <Logo />
+      <HomeLayout>
+        <div className={styles['hero-content-wrap']}>
+          <div className={styles['hero-content']}>
+            <LogoWhiteNoText style={{ width: '6em', height: '6em' }} />
+            <div style={{ textAlign: 'center', fontWeight: 700, fontSize: '3.5em' }}>Your own media universe.</div>
+            <button
+              style={{
+                backgroundColor: 'purple',
+                padding: '0.5em',
+                paddingLeft: '1.5em',
+                paddingRight: '1.5em',
+                borderRadius: '2em',
+                border: 0,
+                display: 'flex',
+                alignItems: 'center',
+                gap: '0.5em'
+              }}
+            >
+              <ArrowDownIcon />
+              <div style={{ fontFamily: 'Figtree', fontWeight: 600, fontSize: '1.1em' }}>Install Jellyfin</div>
+            </button>
+          </div>
+          <div className={styles['features-content']}>
+            <div style={{ textAlign: 'center', fontWeight: 700, fontSize: '1.5em' }}>Jellyfin is...</div>
+            <div style={{ height: '10vh' }}>Jellyfin is...</div>
           </div>
         </div>
-        <div style={{ display: 'flex', gap: '2em' }}>
-          {links.map((link) => (
-            <a href={link.href} style={{ color: 'white' }}>
-              {link.name}
-            </a>
-          ))}
-        </div>
-        <div style={{ display: 'flex' }}>
-          <button
-            style={{
-              backgroundColor: 'transparent',
-              padding: '0.2em',
-              paddingLeft: '1.5em',
-              paddingRight: '1.5em',
-              fontFamily: 'Figtree',
-              fontSize: '1em',
-              borderRadius: '2em',
-              border: 0
-            }}
-          >
-            Try demo
-          </button>
-          <button
-            style={{
-              backgroundColor: 'purple',
-              padding: '0.4em',
-              paddingLeft: '1.5em',
-              paddingRight: '1.5em',
-              fontFamily: 'Figtree',
-              fontWeight: 500,
-              fontSize: '1em',
-              borderRadius: '2em',
-              border: 0
-            }}
-          >
-            Install
-          </button>
-        </div>
-      </div>
-      {/* content */}
-      <div className={styles['hero-content-wrap']}>
-        <div className={styles['hero-content']}>
-          <LogoWhiteNoText style={{ width: '6em', height: '6em' }} />
-          <div style={{ textAlign: 'center', fontWeight: 700, fontSize: '3.5em' }}>Your own media universe.</div>
-          <button
-            style={{
-              backgroundColor: 'purple',
-              padding: '0.5em',
-              paddingLeft: '1.5em',
-              paddingRight: '1.5em',
-              borderRadius: '2em',
-              border: 0,
-              display: 'flex',
-              alignItems: 'center',
-              gap: '0.5em'
-            }}
-          >
-            <ArrowDownIcon />
-            <div style={{ fontFamily: 'Figtree', fontWeight: 600, fontSize: '1.1em' }}>Install Jellyfin</div>
-          </button>
-        </div>
-        <div className={styles['features-content']}>
-          <div style={{ textAlign: 'center', fontWeight: 700, fontSize: '1.5em' }}>Jellyfin is...</div>
-          <div style={{ height: '10vh' }}>Jellyfin is...</div>
-        </div>
-      </div>
-      <Hero title='The Free Software Media System' large>
+      </HomeLayout>
+      {/* <Hero title='The Free Software Media System' large>
         <p className='hero__text margin-vert--lg'>
           Jellyfin is the volunteer-built media solution that puts <em>you</em> in control of your media. Stream to any
           device from your own server, with no strings attached. Your media, your server, your way.
@@ -127,7 +75,7 @@ export default function Home() {
         <InActionSection />
         <FreedomSection />
         <CallToAction />
-      </main>
+      </main> */}
     </Layout>
   );
 }
